4bf71e21e21953d44072ddc1d48a22408bd655a4
lefrostx <lefrostx@gmail.com>
2016-08-24 19:19:26 +0500

    Добавлены таймер, статусы и их функции обработчики.

--- seabattleclient/mainwindow.cpp
+++ seabattleclient/mainwindow.cpp
@@ -6,12 +6,66 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QPalette pal;
-    pal.setBrush(ui->pictureMain->backgroundRole(), QBrush(QPixmap(":/start.png")));
-    ui->pictureMain->setPalette(pal);
+
+    timer = new QTimer{this};
+    connect(timer, &QTimer::timeout, this, &MainWindow::updateStatus);
+
+    prepare();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::updateStatus()
+{
+    switch(status) {
+        case Status::init: doInit(); break;
+        case Status::wait: doWait(); break;
+        case Status::ship: doShip(); break;
+        case Status::load: doLoad(); break;
+        case Status::play: doPlay(); break;
+        case Status::stop: doStop(); break;
+    }
+}
+
+void MainWindow::prepare()
+{
+    status = Status::init;
+    timer->start(500);
+}
+
+void MainWindow::doInit()
+{
+    QPalette pal;
+    pal.setBrush(ui->pictureMain->backgroundRole(), QBrush(QPixmap(":/start.png")));
+    ui->pictureMain->setPalette(pal);
+
+    status = Status::wait;
+}
+
+void MainWindow::doWait()
+{
+
+}
+
+void MainWindow::doShip()
+{
+
+}
+
+void MainWindow::doLoad()
+{
+
+}
+
+void MainWindow::doPlay()
+{
+
+}
+
+void MainWindow::doStop()
+{
+
 }
--- seabattleclient/mainwindow.h
+++ seabattleclient/mainwindow.h
@@ -2,10 +2,20 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
 }
+
+enum class Status {
+    init,
+    wait,
+    ship,
+    load,
+    play,
+    stop
+};
 
 class MainWindow : public QMainWindow
 {
@@ -15,8 +25,21 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void updateStatus();
+
 private:
+    void prepare();
+    void doInit();
+    void doWait();
+    void doShip();
+    void doLoad();
+    void doPlay();
+    void doStop();
+
     Ui::MainWindow *ui;
+    QTimer* timer;
+    Status status{Status::init};
 };
 
 #endif // MAINWINDOW_H
